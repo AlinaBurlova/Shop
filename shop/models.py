6fd3e12762a41db7966c868d0a346e7591ca8061
Alina <alina-burlova@mail.ru>
2024-10-09 21:47:50 +0300

    Commit by 09.10

--- shop/models.py
+++ shop/models.py
@@ -5,7 +5,7 @@ class Category(models.Model):
     name = models.CharField(max_length=200, verbose_name="Категория")
     slug = models.SlugField(max_length=200, unique=True)
 
-    class MEta:
+    class Meta:
         ordering = ['name']
         indexes = [
             models.Index(fields=['name']),
@@ -15,6 +15,7 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
 
 class Product(models.Model):
     category = models.ForeignKey(Category,
@@ -27,7 +28,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
-    update = models.DateTimeField(auto_now=True)
+    updated = models.DateTimeField(auto_now=True)
 
     class Meta:
         ordering = ['name']
@@ -36,6 +37,8 @@ class Product(models.Model):
             models.Index(fields=['name']),
             models.Index(fields=['-created']),
         ]
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
 
     def __str__(self):
         return self.name
